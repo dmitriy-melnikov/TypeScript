66f5cba0f1f47242cd94a56c8066c64a713f9a72
Dzmitry_Melnikau1 <dimaira2012@yandex.ru>
2020-06-25 16:48:26 +0300

    #2

--- script.ts
+++ script.ts
@@ -1,4 +1,10 @@
 console.log('hello tsc');
 console.log('hello second');
+let a = null;
+let b: null = null;
+let c: undefined = undefined;
+let f: object = {};
+let g: {text: string} = {text: 'hello'};
+
 
 //console.log(1 + true);
