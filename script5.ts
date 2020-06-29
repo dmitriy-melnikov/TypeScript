8d958154a241fecb42c9a2dc0a33a508a72193b1
Dzmitry_Melnikau1 <dimaira2012@yandex.ru>
2020-06-29 20:16:06 +0300

    #5

---
+++ script5.ts
@@ -0,0 +1,21 @@
+//const w: (number | string)[] = [1,2,3];
+//const w: Array<number | string> = [1,2,3];
+
+// кортежи
+
+//let w: [number, string] = [1, '', 1]; error
+//let w: [number, string?] = [1, 2];
+
+//перечисление
+/*enum G {
+    A = 5,
+    B
+}*/
+enum G {
+    A = 'Hello',
+    B = 'TS'
+}
+
+//console.log(G.A, G.B, G[5], G[6]);
+//console.log(G.A, G.B, G[G.A], G[G.B]);
+
