5803a8b6221fdf5689a55ad8496d1f5cdee41673
Dzmitry_Melnikau1 <dimaira2012@yandex.ru>
2020-07-10 09:12:00 +0300

    #14 - p1

--- utils.ts
+++ utils.ts
@@ -14,3 +14,16 @@ type Animal3 = MyRequired<Animal1>
 type Color1 = string | undefined | null;
 type Color2 = NonNullable<Color1>
 type MyNonNullable<T> = T extends null | undefined ? never : T
+
+// Record
+type Dimension1 = {width: number, height: number, length: number}
+type Dimension2 = Record<'width' | 'height' | 'length', number>
+type MyRecord<K extends keyof any, T> = { [P in K]: T }
+type Dimension3 = MyRecord<'width' | 'height' | 'length', number>
+
+// Readonly
+type Article1 = {title: string, page: number};
+type Article2 = Readonly<Article1>
+const article1: Article1 = {title: 'hell', page: 10}
+const article2: Article2 = {title: 'hell', page: 10}
+//article2.title = 'hell2' // - error
